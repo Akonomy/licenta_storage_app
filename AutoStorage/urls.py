f566074f66034a1b1aa7c5989a8b6409d41f7bbf
akonomy <andrei.spac@student.usv.ro>
2025-02-19 12:24:15 +0200

    for debug purpose

--- AutoStorage/urls.py
+++ AutoStorage/urls.py
@@ -16,6 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.core.urls')),  # aplicația principală
@@ -28,3 +36,5 @@ urlpatterns = [
 ]
 
 
+if settings.DEBUG:  # Doar în modul de dezvoltare
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
